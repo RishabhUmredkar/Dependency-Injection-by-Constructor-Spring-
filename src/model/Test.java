2a6da253e1cef990a7308e196113da452cbbcf54
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-08-31 11:02:50 +0530

    first commit

---
+++ src/model/Test.java
@@ -0,0 +1,16 @@
+package model;
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.*;  
+  
+public class Test {  
+    public static void main(String[] args) {  
+          
+        Resource r=new ClassPathResource("applicationContext.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+          
+        Employee s=(Employee)factory.getBean("e");  
+        s.show();  
+          
+    }  
+}  
